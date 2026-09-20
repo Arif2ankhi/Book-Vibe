257590cb8ee40159eb7012991044084c96b31124
Arif2ankhi <arif2ankhi@gmail.com>
2026-09-20 18:41:15 +0100

    .envs added

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -7,7 +7,7 @@ const getBooks = async()=>{
 
   try{
      const response = await fetch
-  (`${process.env.NEXT_PUBLIC_SERVER_BASE_URL}}/booksData.json`);
+  (`${process.env.NEXT_PUBLIC_SERVER_BASE_URL}}/booksData.json`,);
    const data = await response.json();
   return data;
 
--- src/context/BooksContext.tsx
+++ src/context/BooksContext.tsx
@@ -1,12 +1,24 @@
 
 'use client'
+import { IBook } from '@/type/books.type';
 import React, { createContext, ReactNode, useState } from 'react';
+interface IBookContext {
+    readBooks: IBook[];
+    setReadBooks:React.Dispatch<React.SetStateAction<IBook[]>>;
+    wishlist: IBook[];
+    setWishlist: React.Dispatch<React.SetStateAction<IBook[]>>;
+}
+ export const BooksContext = createContext<IBookContext >({
+    readBooks: [],
+    setReadBooks:()=> {},
+     wishlist: [],
+    setWishlist:()=> {},
 
- export const BooksContext = createContext({})
+ })
 const BooksProvider = ({children}:{children:ReactNode}) => {
 
-const [readBooks, setReadBooks]= useState([]);
-const [wishlist, setWishlist]= useState([]);
+const [readBooks, setReadBooks]= useState<IBook[]>([]);
+const [wishlist, setWishlist]= useState <IBook[]>([]);
 
 const sharedData = {
     readBooks,
